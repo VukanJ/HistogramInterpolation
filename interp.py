dba4eee6ff0ab6a21f06553fad6a2268a8919c64
VukanJ <vukan.jevtic@tu-dortmund.de>
2025-07-13 23:49:59 +0200

    Two vialble implementations

--- interp.py
+++ interp.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.interpolate import interp1d
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 
 class LinHist:
     def __init__(self, x: np.ndarray, y: np.ndarray):
@@ -17,7 +17,7 @@ class LinHist:
         return len(self.x)
 
     def eval(self, X) -> np.ndarray:
-        return interp1d(self.x, self.y, bounds_error=False)(X)
+        return np.interp(X, self.x, self.y)
 
     def area(self) -> float:
         return np.sum((self.y[1:] + self.y[:-1]) * (self.x[1:] - self.x[:-1])) * 0.5
@@ -30,14 +30,12 @@ def interp(h1: LinHist, h2: LinHist, alpha: float):
     cdf1 = h1.cdf()
     cdf2 = h2.cdf()
 
+    # Check whether cdf1 or cdf2 has more y values
+
     xspace = cdf1.x
 
-    # x1 = cdf1.x  # definition
-    # x2 = LinHist(cdf2.y, cdf2.x).eval(cdf1.eval(x1))  # x2 = CDF2^-1(CDF1(x1))
-    ally = np.linspace(0, 1, len(xspace))
-
-    x1 = LinHist(cdf1.y, cdf1.x).eval(ally)  # x2 = CDF2^-1(CDF1(x1))
-    x2 = LinHist(cdf2.y, cdf2.x).eval(ally)  # x2 = CDF2^-1(CDF1(x1))
+    x1 = h1.x
+    x2 = LinHist(cdf2.y, cdf2.x).eval(cdf1.y)
 
     f1_x1 = LinHist(xspace, h1.y).eval(x1)
     f2_x2 = LinHist(xspace, h2.y).eval(x2)
@@ -46,15 +44,36 @@ def interp(h1: LinHist, h2: LinHist, alpha: float):
     D = (1.0 - alpha) * f2_x2 + alpha * f1_x1
     D[D == 0] = 1
     f /= D
-    # return LinHist(xspace, f * ((1-alpha) * h1.area() + alpha * h2.area()))
-    return LinHist((x1 * (1-alpha) + x2 * alpha), f)
+    return LinHist(x1 * (1-alpha) + alpha* x2, f)
+
+# def interp(h1: LinHist, h2: LinHist, alpha: float):
+#     assert len(h1) == len(h2)
+#     assert len(h1.x) == len(h2.x)
+#
+#     cdf1 = h1.cdf()
+#     cdf2 = h2.cdf()
+#
+#     yspace = np.linspace(0, 1, 100)
+#     xspace = np.linspace(h1.x[0], h1.x[-1], 100)
+#
+#     x1 = LinHist(cdf1.y, cdf1.x).eval(yspace)
+#     x2 = LinHist(cdf2.y, cdf2.x).eval(yspace)
+#
+#     f1_x1 = LinHist(xspace, h1.eval(xspace)).eval(x1)
+#     f2_x2 = LinHist(xspace, h2.eval(xspace)).eval(x2)
+#
+#     f = f1_x1 * f2_x2
+#     D = (1.0 - alpha) * f2_x2 + alpha * f1_x1
+#     D[D == 0] = 1
+#     f /= D
+#     return LinHist(x1 * (1-alpha) + alpha* x2, f)
 
 
-data1 = np.random.normal(loc=-1.3, scale=0.3, size=100000)
+data1 = np.random.normal(loc=-1.3, scale=0.05, size=100000)
 data2 = np.random.normal(loc=0.7, scale=0.6, size=100000)
 
 hrange = (-3, 3)
-nbins = 20
+nbins = 40
 h1, e1 = np.histogram(data1, bins=nbins, range=hrange)
 h2, e2 = np.histogram(data2, bins=nbins, range=hrange)
 h1 = np.array(h1, dtype=np.float32)
@@ -67,7 +86,7 @@ H1 = LinHist(e1, h1)
 H2 = LinHist(e2, h2)
 xhighres = np.linspace(*hrange, 1000)
 
-HI = interp(H1, H2, 0.5)
+HI = interp(H1, H2, 0.01)
 
 plt.subplot(2, 1, 1)
 plt.step(H1.x, H1.y, where="mid")
@@ -75,8 +94,11 @@ plt.plot(xhighres, H1.eval(xhighres), linewidth=0.5, color="k")
 plt.step(H2.x, H2.y, where="mid")
 plt.plot(xhighres, H2.eval(xhighres), linewidth=0.5, color="k")
 plt.step(H1.x, HI.eval(H1.x), where="mid")
+print(HI.eval(H1.x))
 plt.plot(xhighres, HI.eval(xhighres), linewidth=0.5, color="k")
 plt.ylabel("Counts")
+
+
 plt.subplot(2, 1, 2)
 cdf1 = H1.cdf()
 cdf2 = H2.cdf()
